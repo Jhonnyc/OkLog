1f46bc724c064d831b457cb1fb7322853d2044a4
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-11 23:37:15 +0300

    writing to the log file on a seperate thread.

---
+++ OkLog/src/com/oklog/WriterThread.java
@@ -0,0 +1,49 @@
+package com.oklog;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
+
+public class WriterThread implements Runnable {
+
+	private RingQueue mRingQueue;
+	private File mLogFile;
+	private LogLine mLog;
+	private boolean mAppendQueue = false;
+
+	public WriterThread(RingQueue ringQueue,  File logFile) {
+		mRingQueue = ringQueue;
+		mLogFile = logFile;
+		mAppendQueue = true;
+	}
+	
+	public WriterThread(LogLine log, File logFile) {
+		mLog = log;
+		mLogFile = logFile;
+	}
+
+	@Override
+	public void run() {
+//		long start = System.currentTimeMillis();
+		FileWriter writer = null;
+
+		try {
+			String content;
+			writer = new FileWriter(mLogFile, false);
+			if(mAppendQueue) {
+				content = mRingQueue.getLogFileBody();
+			} else {
+				content = mLog.toString();
+			}
+			writer.close();
+//			long end = (System.currentTimeMillis() - start) / 1000;
+//			Log.e(WriterThread.class.getSimpleName(), "Took " + end + " Sec");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
