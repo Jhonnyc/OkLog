8210d18b32ac67c2bc60dc40a11df8873aa51044
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-14 07:51:08 +0300

    Added two classes to support running a queue of runnables.

---
+++ OkLog/src/com/oklog/TaskStatusListener.java
@@ -0,0 +1,7 @@
+package com.oklog;
+
+public interface TaskStatusListener {
+
+	public void onTaskComplete();
+	
+}
---
+++ OkLog/src/com/oklog/TaskStatusReporter.java
@@ -0,0 +1,7 @@
+package com.oklog;
+
+public interface TaskStatusReporter {
+	
+	public void onTaskDone();
+
+}
