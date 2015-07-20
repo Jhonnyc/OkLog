ec53120450ae6668296f370ec0e60e8e940cc734
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-20 09:06:35 +0300

    Added a queue of runnables to run read\write tasks asynchronously (1 task in parallel to the main UI thread)

---
+++ OkLog/src/com/oklog/entities/RunnablesQueue.java
@@ -0,0 +1,39 @@
+package com.oklog.entities;
+
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.oklog.interfaces.TaskStatusListener;
+
+public class RunnablesQueue implements TaskStatusListener {
+	
+	private LinkedList<Runnable> mLinkedList;
+	private AtomicBoolean mIsBusy;
+	
+	public RunnablesQueue() {
+		mLinkedList = new LinkedList<Runnable>();
+		mIsBusy = new AtomicBoolean(false);
+	}
+	
+	public synchronized void addTask(Runnable task) {
+		if(mIsBusy.get()) {
+			mLinkedList.add(task);
+		} else {
+			mIsBusy.set(true);
+			task.run();
+		}
+	}
+	
+	@Override
+	public synchronized void onTaskComplete() {
+		mIsBusy.set(false);
+		Runnable task = mLinkedList.pop();
+		if(task != null) {
+			mIsBusy.set(true);
+			task.run();
+		}
+	}
+	
+	
+
+}
