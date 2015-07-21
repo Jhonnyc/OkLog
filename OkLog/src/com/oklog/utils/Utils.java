2b1e9edb326153ca2f735d198fce1bd33a12b9f7
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-22 00:06:36 +0300

    Aligning the stacktrace elements under each other for readability

--- OkLog/src/com/oklog/utils/Utils.java
+++ OkLog/src/com/oklog/utils/Utils.java
@@ -3,12 +3,16 @@ package com.oklog.utils;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import android.annotation.SuppressLint;
+
 import com.oklog.OkLog;
+import com.oklog.entities.StringKeeper;
 
 public class Utils {
 	
 	public static final String NEW_LINE = System.getProperty("line.separator");
 
+	@SuppressLint("SimpleDateFormat") 
 	public static String getStringDateFromEpochTime(long epochTime) {
 		Date date = new Date(epochTime);
 		SimpleDateFormat formatter = new SimpleDateFormat(OkLog.Configuration.getTimeFormat().toString());
@@ -26,5 +30,32 @@ public class Utils {
 		return stringBuilder.toString();
 	}
 	
+	public static StringKeeper getFormattedStackTraceForException(Throwable exception, int pad) {
+		String causeLine;
+		StringKeeper stringKeeper = new StringKeeper();
+		StackTraceElement[] elements = exception.getStackTrace();
+		if(elements != null && elements.length > 0) {
+			stringKeeper.appendLine(elements[0].toString());
+			if(elements.length > 1) {
+				for (int i = 1; i < (elements.length - 1); i++) {
+					String element = elements[i].toString();
+					causeLine = Utils.padLeft(element,pad + element.length());
+					stringKeeper.appendLine(causeLine);
+				}
+				String element = elements[elements.length - 1].toString();
+				causeLine = Utils.padLeft(element,pad + element.length());
+				stringKeeper.appendLine(causeLine);
+			}
+		}
+		
+		return stringKeeper;
+	}
 	
+	public static String padRight(String s, int n) {
+	     return String.format("%-" + n + "s", s);  
+	}
+
+	public static String padLeft(String s, int n) {
+	    return String.format("%" + n + "s", s);  
+	}
 }
