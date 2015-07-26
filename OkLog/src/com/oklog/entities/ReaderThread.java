858f99b2cbc839761316e6af969c0fbb906ebdfb
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-26 19:39:10 +0300

    Removed unused import

--- OkLog/src/com/oklog/entities/ReaderThread.java
+++ OkLog/src/com/oklog/entities/ReaderThread.java
@@ -3,7 +3,6 @@ package com.oklog.entities;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
