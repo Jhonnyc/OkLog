f9e75f45b24909982262db2bec86e347280f8baa
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-26 19:08:31 +0300

    changed functionality

--- OkLog/src/com/oklog/entities/StringKeeper.java
+++ OkLog/src/com/oklog/entities/StringKeeper.java
@@ -1,23 +1,54 @@
 package com.oklog.entities;
 
-
 public class StringKeeper {
 	
 	private StringBuilder mStringBuilder;
 	private int mNumberOfLines;
 	
 	public StringKeeper(int size) {
-		mStringBuilder = new StringBuilder();
 		mNumberOfLines = 0;
+		mStringBuilder = new StringBuilder();
+	}
+	
+	public StringKeeper() {
+		mNumberOfLines = 0;
+		mStringBuilder = new StringBuilder();
+	}
+	
+	public void addLine() {
+		mNumberOfLines++;
+	}
+	
+
+	public void removeLine() {
+		mNumberOfLines--;
 	}
 
 	public void append(String line) {
-		mNumberOfLines++;
 		mStringBuilder.append(line);
+	}
+	
+	public void appendLine(String line) {
+		mNumberOfLines++;
+		mStringBuilder.append(String.format("%s%n", line));
+	}
+	
+	public void append(StringKeeper line) {
+		mNumberOfLines += line.getNumberOfLines();
+		mStringBuilder.append(line.toString());
+	}
+	
+	public void appendFormat(String format, StringKeeper line) {
+		mNumberOfLines += line.getNumberOfLines();
+		mStringBuilder.append(String.format(format, line.toString()));
 	}
 	
 	public int getNumberOfLines() {
 		return mNumberOfLines;
+	}
+	
+	public int length() {
+		return mStringBuilder.length();
 	}
 	
 	@Override
