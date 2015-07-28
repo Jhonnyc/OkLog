bd3c0be7906713be12249ac6da4f465cd6fb079d
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-07-28 19:01:14 +0300

    Added validation for setters

--- OkLog/src/com/oklog/config/LogConfiguration.java
+++ OkLog/src/com/oklog/config/LogConfiguration.java
@@ -1,59 +1,93 @@
 package com.oklog.config;
 
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class LogConfiguration {
 
 	// Class Variables
-	private boolean mPrintStackTrace;
-	private boolean mSendUsingEmail;
+	private boolean mRetryOnFail;
 	private int mSize;
+	private int mLogCycle;
+	private int mThreadPoolSize;
 	private String mLogFileName;
 	private TimeFormat mTimeFormat;
-	private LogLevel mLogLevel;
-	private FilePolicy mFilePolicy;
 	private WritePolicy mWritePolicy;
+	private List<LogLevel> mLogLevelList;
+	private List<LogLevel> mStackTraceList;
 
 	public LogConfiguration() {
-		mPrintStackTrace = true;
-		mSendUsingEmail = false;
+		mRetryOnFail = true;
 		mSize = 1000;
+		mLogCycle = 100;
+		mThreadPoolSize = mSize + 1; // Since there is one reader thread running once we start
 		mLogFileName = "OkLog.log";
 		mTimeFormat = TimeFormat.DD_MM_YYYY__HH_mm_ss_SSS;
-		mLogLevel = LogLevel.VERBOSE;
-		mFilePolicy = FilePolicy.WRITE_ALL;
-		mWritePolicy = WritePolicy.EVERY_LINE;
+		mWritePolicy = WritePolicy.EVERY_LOG_CYCLE;
+		mLogLevelList = LogLevel.asList();
+		mStackTraceList = new ArrayList<LogLevel>();
+		mStackTraceList.add(LogLevel.ERROR);
 	}
 
-	public boolean getPrintStackTrace() {
-		return mPrintStackTrace;
+	public boolean getRetryOnFail() {
+		return mRetryOnFail;
 	}
 	
-	public void setPrintStackTrace(boolean printStackTrace) {
-		mPrintStackTrace = printStackTrace;
-	}
-	
-	public boolean getSendEmail() {
-		return mSendUsingEmail;
-	}
-	
-	public void setSendUsingEmail(boolean sendUsingEmail) {
-		mSendUsingEmail = sendUsingEmail;
+	public void setRetryOnFail(boolean retryOnFail) {
+		mRetryOnFail = retryOnFail;
 	}
 	
 	public int getSize() {
 		return mSize;
 	}
 	
-	public void setSize(int size) {
-		mSize = size;
+	public void setSize(int size) throws IllegalArgumentException {
+		if(size < 1) {
+			throw new IllegalArgumentException("Size has to be a positive number");
+		} else {
+			mSize = size;
+		}
+	}
+	
+	public int getLogCycleSize() {
+		return mLogCycle;
+	}
+	
+	public void setLogCycleSize(int numberOfCycles) throws IllegalArgumentException {
+		if(numberOfCycles < 1) {
+			throw new IllegalArgumentException("Cycles has to be a positive number");
+		} else {
+			mLogCycle = numberOfCycles;
+		}
+	}
+	
+	public int getThreadPoolSize() {
+		return mThreadPoolSize;
+	}
+	
+	public void setThreadPoolSize(int threadPoolSize) throws IllegalArgumentException {
+		if(threadPoolSize < 1) {
+			throw new IllegalArgumentException("Thread pool size has to be a positive number");
+		} else {
+			mThreadPoolSize = threadPoolSize;
+		}
 	}
 	
 	public String getLogFileName() {
 		return mLogFileName;
 	}
 
-	public void setLogFileName(String logFileName) {
-		mLogFileName = logFileName;
+	public void setLogFileName(String logFileName) throws InvalidParameterException, IllegalArgumentException {
+		if(logFileName == null || logFileName.trim().length() < 3 || !logFileName.contains(".")) {
+			throw new InvalidParameterException("Log file name has to be in the form of <name>.[<log> or <txt>]");
+		} else if(!logFileName.subSequence(logFileName.indexOf("."), logFileName.length()).equals("txt") &&
+				!logFileName.subSequence(logFileName.indexOf("."), logFileName.length()).equals("log")) {
+			throw new InvalidParameterException("Log file type can only be <log> or <txt>");
+		} else {
+			mLogFileName = logFileName;
+		}
 	}
 	
 	public TimeFormat getTimeFormat() {
@@ -64,27 +98,42 @@ public class LogConfiguration {
 		mTimeFormat = timeFormat;
 	}
 	
-	public LogLevel getLogLevel() {
-		return mLogLevel;
-	}
-
-	public void setLogLevel(LogLevel logLevel) {
-		mLogLevel = logLevel;
-	}
-	
-	public FilePolicy getFilePolicy() {
-		return mFilePolicy;
-	}
-	
-	public void setFilePolicy(FilePolicy filePolicy) {
-		mFilePolicy = filePolicy;
-	}
-	
 	public WritePolicy getWritePolicy() {
 		return mWritePolicy;
 	}
 	
 	public void setWritePolicy(WritePolicy writePolicy) {
 		mWritePolicy = writePolicy;
+	}
+	
+	public List<LogLevel> getLevelsToLog() {
+		return mLogLevelList;
+	}
+	
+	public void setLevelsTolog(List<LogLevel> levelsTolog) throws IllegalArgumentException {
+		if(levelsTolog.size() < 1) {
+			throw new IllegalArgumentException("The list of levels to log cannot be empty");
+		} else {
+			mLogLevelList = levelsTolog;
+		}
+	}
+	
+	public List<LogLevel> getLevelsForStackTracePrint() {
+		return mStackTraceList;
+	}
+	
+	public void setStackTraceList(List<LogLevel> levelList) throws IllegalArgumentException {
+		if(levelList == null) {
+			levelList = new ArrayList<LogLevel>();
+		} else if(levelList.size() > 0) {
+			for(LogLevel level : levelList) {
+				if(!mLogLevelList.contains(level)) {
+					throw new IllegalArgumentException("You can only print the " +
+							"stack trace of levels you want to log");
+				}
+			}
+		}
+		
+		mStackTraceList = levelList;
 	}
 }
