f973864c5e05c00cc76678e7b7910997c7b4b992
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-08-01 22:37:44 +0300

    Method changes.

--- OkLog/src/com/oklog/entities/RingBuffer.java
+++ OkLog/src/com/oklog/entities/RingBuffer.java
@@ -6,11 +6,13 @@ public class RingBuffer {
 	
 	// Class variables
 	private int mSize;
+	private long mNumberOfElements;
 	private Object mLock;
 	private ConcurrentLinkedQueue<LogLine> mElements; 
 	
 	public RingBuffer(int size) {
 		mSize = size;
+		mNumberOfElements = 0;
 		mLock = new Object();
 		mElements = new ConcurrentLinkedQueue<LogLine>();
 	}
@@ -20,12 +22,14 @@ public class RingBuffer {
 			while(mElements.size() >= mSize) {
 				mElements.poll();
 			}
+			mNumberOfElements++;
 			mElements.add(log);
 		}
 	}
 	
 	public void reset() {
 		synchronized (mLock) {
+			mNumberOfElements = 0;
 			mElements.clear();
 		}
 	}
@@ -35,9 +39,13 @@ public class RingBuffer {
 		synchronized (mLock) {
 			logTextBody = new StringBuilder();
 			for(LogLine log : mElements) {
-				logTextBody.append(String.format("%s%n", log.toString()));
+				logTextBody.append(String.format("%s%n", log.getString()));
 			}
 		}
 		return logTextBody.toString();
+	}
+	
+	public long size() {
+		return mNumberOfElements;
 	}
 }
