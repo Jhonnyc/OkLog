172f913ef5659eec3c746da50805bf09a0edc687
Yoni Kalangan <yoni.kalangan@gmail.com>
2015-08-03 09:00:56 +0300

    Using thread pool executor rather then a queue of runnables to synchronize running threads.

--- OkLog/src/com/oklog/OkLog.java
+++ OkLog/src/com/oklog/OkLog.java
@@ -1,7 +1,12 @@
 package com.oklog;
 
 import java.io.File;
-import java.io.FileOutputStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import android.content.Context;
 import android.net.Uri;
@@ -12,38 +17,63 @@ import com.oklog.config.LogConfiguration;
 import com.oklog.config.LogLevel;
 import com.oklog.config.WritePolicy;
 import com.oklog.entities.LogLine;
+import com.oklog.entities.OnAfterCrash;
 import com.oklog.entities.ReaderThread;
-import com.oklog.entities.RingQueue;
-import com.oklog.entities.RunnablesQueue;
+import com.oklog.entities.RingBuffer;
 import com.oklog.entities.StringKeeper;
 import com.oklog.entities.WriterThread;
 
 public class OkLog {
 
+	// Class final fields
+	private static final String LOG_FILE_NAME = "Log.log";
+	
 	// Class private fields
 	private Context mContext;
 	private static OkLog mInstance;
 	private static boolean mInitiated = false;
-	private RingQueue mLogs;
 	private StringKeeper mStringKeeper; 
-	private static RunnablesQueue mRunnablesQueue;
+	private BlockingQueue<Runnable> mBlockingQueue;
+	private static OnAfterCrash mOnAfterCrash;  
 	private static File mLogFile;
+	private static File mLogFileToSend;
+	private static ThreadPoolExecutor mExecutor;
+	private static AtomicInteger mCurrentNumberOfLines;
+	private static RingBuffer mLogs;
 	
 	// Class public fields
 	public static LogConfiguration Configuration = new LogConfiguration();
 	
 	private OkLog(Context context) {
 		mContext = context;
-		mLogs = new RingQueue(Configuration.getSize());
-		mLogFile = getLogFile(Configuration.getLogFileName(), mContext);
+		mLogFile = getLogFile(LOG_FILE_NAME, mContext);
+		mLogFileToSend = getLogFile(Configuration.getLogFileName(), mContext);
 		mStringKeeper = new StringKeeper(Configuration.getSize());
-		ReaderThread reader = new ReaderThread(mLogFile, mStringKeeper, mRunnablesQueue);
-		mRunnablesQueue = new RunnablesQueue();
-		mRunnablesQueue.addTask(reader);
+		mLogs = new RingBuffer(Configuration.getSize());
+		mCurrentNumberOfLines = new AtomicInteger();
+		ReaderThread reader = new ReaderThread(mLogFile, mLogFileToSend, mStringKeeper, mOnAfterCrash);
+		mBlockingQueue = new ArrayBlockingQueue<Runnable>(Configuration.getThreadPoolSize());
+		mExecutor = new ThreadPoolExecutor(1, 1, 5000, TimeUnit.SECONDS, mBlockingQueue);
+		mExecutor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+			@Override
+			public void rejectedExecution(Runnable runnable, ThreadPoolExecutor executor) {
+				if(Configuration.getRetryOnFail()) {
+					executor.execute(runnable);
+				}
+			}
+		});
+		mExecutor.execute(reader);
 	}
 	
 	public synchronized static void initialize(Context context) {
 		mInitiated = true;
+		mOnAfterCrash = null;
+		mInstance = new OkLog(context);
+	}
+	
+	public synchronized static void initialize(Context context, OnAfterCrash onAfterCrash) {
+		mInitiated = true;
+		mOnAfterCrash = onAfterCrash;
 		mInstance = new OkLog(context);
 	}
 	
@@ -57,7 +87,6 @@ public class OkLog {
 	
 	public void setConfiguration(LogConfiguration configuration) {
 		Configuration = configuration;
-		mLogs = new RingQueue(Configuration.getSize());
 	}
 	
 	/**********************************
@@ -168,88 +197,16 @@ public class OkLog {
 	
 	/**********************************
 	***********************************
-	********* Private Methods *********
+	********* Public Methods **********
 	***********************************
 	***********************************/
 	
-	private static <T> void logByTag(LogLevel level, String tag, String msg, T... args) throws NullPointerException {
-		if(!mInitiated) {
-			throw new NullPointerException("The class has never been initialized. " +
-					"Use initialize(context) first to create a new instance");
-		}
-		LogLine log = new LogLine(level, String.format(msg, args), null, Configuration.getPrintStackTrace());
-		logToLogcat(level, tag, String.format(msg, args));
-		writeOrAddToQueue(log);
-	}
-	
-	private static <T> void logByClass(LogLevel level, Class<?> clazz, String msg, Exception exception) throws NullPointerException {
-		if(!mInitiated) {
-			throw new NullPointerException("The class has never been initialized. " +
-					"Use initialize(context) first to create a new instance");
-		}
-		LogLine log = new LogLine(level, clazz, msg, exception, Configuration.getPrintStackTrace());
-		logToLogcat(level, clazz.getSimpleName(), msg);
-		writeOrAddToQueue(log);
-	}
-	
-	private static <T> void logByObject(LogLevel level, Object object, String msg, Exception exception) throws NullPointerException {
-		if(!mInitiated) {
-			throw new NullPointerException("The class has never been initialized. " +
-					"Use initialize(context) first to create a new instance");
-		}
-		LogLine log = new LogLine(level, object, msg, exception, Configuration.getPrintStackTrace());
-		logToLogcat(level, object.getClass().getSimpleName(), msg);
-		writeOrAddToQueue(log);
-	}
-	
-	private static void logToLogcat(LogLevel level, String tag, String msg) {
-		switch (level) {
-		case DEBUG:
-			Log.d(tag, msg);
-			break;
-		case ERROR:
-			Log.e(tag, msg);
-			break;
-		case INFO:
-			Log.i(tag, msg);
-			break;
-		case VERBOSE:
-			Log.v(tag, msg);
-			break;
-		case WARNING:
-			Log.w(tag, msg);
-			break;
-		default:
-			Log.e(tag, msg);
-			break;
-		}
-	}
-	
-	private static synchronized void writeOrAddToQueue(final LogLine log) {
-		if(Configuration.getWritePolicy().equals(WritePolicy.EVERY_LINE)) {
-			WriterThread writer = new WriterThread(log, mLogFile, mRunnablesQueue);
-			mRunnablesQueue.addTask(writer);
-		}
-	}
-	
-	public void writeFile(String fileName, String content) {
-		FileOutputStream outputStream = null;
-		try {
-			outputStream = mContext.openFileOutput(fileName, Context.MODE_PRIVATE);
-			outputStream.write(content.getBytes());
-			outputStream.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public File getLogFile(String fileName, Context context) {
-		boolean created = false;
+	public static File getLogFile(Context context) {
 		File file = null;
 		try {
-		    file = new File(context.getFilesDir(), fileName);
+		    file = new File(context.getFilesDir(), Configuration.getLogFileName());
 		    if(!file.exists()) {
-		    	created = file.createNewFile();
+		    	file.createNewFile();
 		    }
 		} catch (Exception e) {
 		    e.printStackTrace();
@@ -258,9 +215,17 @@ public class OkLog {
 		return file;
 	}
 	
-	public Uri getLogFileUri(String fileName) {
-		Uri uri = Uri.fromFile(getLogFile(Configuration.getLogFileName(), mContext));
+	public static Uri getLogFileUri(Context context) {
+		Uri uri = Uri.fromFile(getLogFile(context));
 		return uri;
+	}
+	
+	public synchronized static int addToCurrentLines(int addition) {
+		return mCurrentNumberOfLines.addAndGet(addition);
+	}
+	
+	public synchronized static int getCurrentLines() {
+		return mCurrentNumberOfLines.get();
 	}
 	
 	/**
@@ -286,5 +251,99 @@ public class OkLog {
 	        return true;
 	    }
 	    return false;
+	}
+	
+	/**********************************
+	***********************************
+	********* Private Methods *********
+	***********************************
+	***********************************/
+	
+	private static <T> void logByTag(LogLevel level, String tag, String msg, T... args) throws NullPointerException {
+		if(!mInitiated) {
+			throw new NullPointerException("The class has never been initialized. " +
+					"Use initialize(context) first to create a new instance");
+		}
+		boolean printStackTrace = Configuration.getLevelsForStackTracePrint().contains(level);
+		LogLine log = new LogLine(level, String.format(msg, args), null, printStackTrace);
+		logToLogcat(level, tag, String.format(msg, args));
+		writeToLog(log);
+	}
+	
+	private static <T> void logByClass(LogLevel level, Class<?> clazz, String msg, Exception exception) throws NullPointerException {
+		if(!mInitiated) {
+			throw new NullPointerException("The class has never been initialized. " +
+					"Use initialize(context) first to create a new instance");
+		}
+		boolean printStackTrace = Configuration.getLevelsForStackTracePrint().contains(level);
+		LogLine log = new LogLine(level, clazz, msg, exception, printStackTrace);
+		logToLogcat(level, clazz.getSimpleName(), msg);
+		writeToLog(log);
+	}
+	
+	private static <T> void logByObject(LogLevel level, Object object, String msg, Exception exception) throws NullPointerException {
+		if(!mInitiated) {
+			throw new NullPointerException("The class has never been initialized. " +
+					"Use initialize(context) first to create a new instance");
+		}
+		boolean printStackTrace = Configuration.getLevelsForStackTracePrint().contains(level);
+		LogLine log = new LogLine(level, object, msg, exception, printStackTrace);
+		logToLogcat(level, object.getClass().getSimpleName(), msg);
+		writeToLog(log);
+	}
+	
+	private static void logToLogcat(LogLevel level, String tag, String msg) {
+		switch (level) {
+		case DEBUG:
+			Log.d(tag, msg);
+			break;
+		case ERROR:
+			Log.e(tag, msg);
+			break;
+		case INFO:
+			Log.i(tag, msg);
+			break;
+		case VERBOSE:
+			Log.v(tag, msg);
+			break;
+		case WARNING:
+			Log.w(tag, msg);
+			break;
+		default:
+			Log.e(tag, msg);
+			break;
+		}
+	}
+	
+	private static synchronized void writeToLog(final LogLine log) {
+		if(Configuration.getLevelsToLog().contains(log.getLevel())) {
+			WriterThread writer;
+			mLogs.add(log);
+			if(Configuration.getWritePolicy().equals(WritePolicy.EVERY_LINE)) {
+				writer = new WriterThread(mLogs, mLogFile);
+				mExecutor.execute(writer);
+			} else if(Configuration.getWritePolicy().equals(WritePolicy.EVERY_LOG_CYCLE)) {
+				boolean isEmpty = mLogs.size() > 0;
+				boolean isCycle = ((mLogs.size() % Configuration.getLogCycleSize()) == 0);
+				if(!isEmpty && isCycle) {
+					writer = new WriterThread(mLogs, mLogFile);
+					mExecutor.execute(writer);
+				}
+			}
+		}
+	}
+	
+	private File getLogFile(String fileName, Context context) {
+		File file = null;
+		try {
+		    file = new File(context.getFilesDir(), fileName);
+		    if(!file.exists()) {
+		    	file.createNewFile();
+		    }
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+		
+		return file;
 	}
 }
